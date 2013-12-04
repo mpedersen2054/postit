da8f0c4756c75bc8608ef961d28ebdcdbe495d44
Matt Pedersen <mpedersen2054@gmail.com>
2013-12-04 02:03:59 -0600

    user model created

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131204075554) do
+ActiveRecord::Schema.define(version: 20131204080144) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.string   "url"
     t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
