bd2a6a6896f0af856d24d9798357c1fa8f7617bc
Matt Pedersen <mpedersen2054@gmail.com>
2013-12-09 16:34:27 -0600

    posts controller complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,54 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.creator = User.first
+
+    if @post.save
+      flash[:notice] = "This Post was Created"
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      flash[:notice] = "This Post was Updated"
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :content)
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
