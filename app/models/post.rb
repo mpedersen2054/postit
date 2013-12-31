5756bec4e118fde661429d9c00a4c1016a5a5bd5
Matt Pedersen <mpedersen2054@gmail.com>
2013-12-30 20:11:55 -0600

    extracted vote behavior into Voteable module

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,10 @@
 class Post < ActiveRecord::Base
+  include Voteable
+
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments
   has_many :post_categories
   has_many :categories, through: :post_categories
-  has_many :votes, as: :voteable
 
   validates :title, presence: true, length: { minimum: 5 }
   validates :content, presence: true
@@ -11,18 +12,6 @@ class Post < ActiveRecord::Base
 
   # activerecord callback
   before_save :generate_slug!
-
-  def total_votes
-    self.upvotes - self.downvotes
-  end
-
-  def upvotes
-    self.votes.where(vote: true).size
-  end
-
-  def downvotes
-    self.votes.where(vote: false).size
-  end
 
   def to_param
     self.slug
