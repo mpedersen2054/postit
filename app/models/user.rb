f1b44a49dc49c913ff406ed9cefd652c54563dbd
Matt Pedersen <mpedersen2054@gmail.com>
2014-01-02 19:55:03 -0600

    add simple admin roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ActiveRecord::Base
   validates :password, presence: true, on: :create, length: { minimum: 5 }
 
   slugable_column :username
+
+  def admin?
+    self.role == 'admin'
+  end
+
+  def moderator?
+    self.role == 'moderator'
+  end
 end
