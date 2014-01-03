6039de5328537ed7662376233569ec2024f13b9c
Matt Pedersen <mpedersen2054@gmail.com>
2014-01-02 19:32:43 -0600

    create voteable_matt gem, removed content from lib/voteable.rb and added the new gem to gemfile

--- lib/voteable.rb
+++ lib/voteable.rb
@@ -1,50 +1,34 @@
 # USING CONCERNS(RAILS WAY)
-module Voteable
-  extend ActiveSupport::Concern
-  included do
-    has_many :votes, as: :voteable
-  end
-
-  def total_votes
-    self.upvotes - self.downvotes
-  end
-
-  def upvotes
-    self.votes.where(vote: true).size
-  end
-
-  def downvotes
-    self.votes.where(vote: false).size
-  end
-end
 
 # USING NORMAL METAPROGRAMMING
-# module Voteable
-#   def self.included(base)
-#     base.send(:include, InstanceMethods)
-#     base.extend ClassMethods
-#     base.class_eval do
-#       my_class_method
-#     end
-#   end
+=begin
+module Voteable
+  def self.included(base)
+    base.send(:include, InstanceMethods)
+    base.extend ClassMethods
+    base.class_eval do
+      my_class_method
+    end
+  end
 
-#   module InstanceMethods
-#     def total_votes
-#       self.upvotes - self.downvotes
-#     end
+  module InstanceMethods
+    def total_votes
+      self.upvotes - self.downvotes
+    end
 
-#     def upvotes
-#       self.votes.where(vote: true).size
-#     end
+    def upvotes
+      self.votes.where(vote: true).size
+    end
 
-#     def downvotes
-#       self.votes.where(vote: false).size
-#     end
-#   end
+    def downvotes
+      self.votes.where(vote: false).size
+    end
+  end
 
-#   module ClassMethods
-#     def my_class_method
-#       has_many :votes, as: :voteable
-#     end
-#   end
-# end
+  module ClassMethods
+    def my_class_method
+      has_many :votes, as: :voteable
+    end
+  end
+end
+=end
